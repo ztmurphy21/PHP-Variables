88e60616761791d05c4b7ab7bdcfbfe0ff72488f
Zachary Murphy <ztmurphy21@gmail.com>
2018-07-30 21:25:47 -0500

    initial
    $_SERVER predefined variable.

---
+++ predefined.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Predefined Variables</title>
+</head>
+<body>
+<pre>
+<?php //script 2.1-predefined.php
+
+//Show the value of the $_SERVER
+print_r($_SERVER);
+?>
+</pre>    
+</body>
+</html>
