abbedf382b0797cf8940cf60b7142eee930d246f
Zachary Murphy <ztmurphy21@gmail.com>
2018-07-30 21:43:28 -0500

    variables

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PVariables</title>
+</head>
+<body>
+<?php //script2.3--variables.php
+    
+    //variable declarations
+    $street ="100 Main Street";
+    $city = "State College";
+    $state = "PA";
+    $zip = 16801;
+    
+    //output
+    print "<p>The address is: <br>$street <br>$city , $state $zip</p>";
+?>
+
+  
+</body>
+</html>
